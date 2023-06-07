c9fb69d2f3159ebe05bda7a39f657417b8a3e193
mraf19 <raflimr1902@gmail.com>
2023-06-07 09:10:40 +0700

    Result Page initialize

--- src/components/parts/Suggestions.jsx
+++ src/components/parts/Suggestions.jsx
@@ -3,19 +3,18 @@ import ListItem from "../elements/ListItem";
 
 const Suggestions = () => {
   const country = useSelector((state) => state.country);
-  console.log(country);
+
   return (
     <ul
       className={`w-full py-[15px] shadow-suggestions ${
         country.loading && "hidden"
       } rounded-md}`}
     >
-      {country.loading !== true && country.countries.length !== 0
-        ? country.countries.map((country, index) => (
-            <ListItem key={`suggestion-${index}`} text={country.name.common} />
-          ))
-        : null}
-      {country.loading !== true && country.error.length !== 0 ? (
+      {country.loading !== true && country.countries.length !== 0 ? (
+        country.countries.map((country, index) => (
+          <ListItem key={`suggestion-${index}`} text={country.name.common} />
+        ))
+      ) : country.loading !== true && country.error.length !== 0 ? (
         <ListItem error />
       ) : null}
     </ul>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,21 +7,25 @@ const HomePage = () => {
   const dispatch = useDispatch();
   const [display, setDisplay] = useState(true);
 
+  const onKeyUpAction = (e) => {
+    if (e.target.value.length !== 0) {
+      dispatch(fetchCountries(e.target.value));
+      setDisplay(true);
+    }
+    if (e.target.value.length === 0) {
+      setDisplay(false);
+    }
+  };
+
   return (
-    <div className="w-3/4 mx-auto flex flex-col items-center justify-center">
+    <div className="w-3/4 mx-auto mt-20 flex flex-col items-center justify-center">
       <h1 className="mb-[10px] text-7xl font-bold leading-header">Country</h1>
       <input
         type="text"
         className="w-full mb-[10px] px-7 py-5 outline-none leading-regular border-charcoal border-[0.5px] rounded-[10px] placeholder:leading-regular focus:border-purple focus:border-[3px] focus:rounded-lg"
         placeholder="Type any country name"
         onKeyUp={(e) => {
-          if (e.target.value.length !== 0) {
-            dispatch(fetchCountries(e.target.value));
-            setDisplay(true);
-          }
-          if (e.target.value.length === 0) {
-            setDisplay(false);
-          }
+          onKeyUpAction(e);
         }}
       />
       {display && <Suggestions />}
