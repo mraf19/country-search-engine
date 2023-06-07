1aaa8d29573ea17f02ea9d2d67237fa142885dcf
mraf19 <raflimr1902@gmail.com>
2023-06-07 07:11:58 +0700

    early config project

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1 className="text-7xl ">Country</h1>
+    </>
+  );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["SF Pro Display", "sans-serif"],
+      },
+      colors: {
+        purple: "#8362F2",
+        charcoal: "#C8C8C8",
+        jadeGreen: "#8DD4CC",
+        darkGrey: "#525252",
+      },
+      lineHeight: {
+        small: "16.71px",
+        regular: "21.48px",
+        large: "57.28px",
+        header: "85.92px",
+      },
+    },
   },
   plugins: [],
 };
